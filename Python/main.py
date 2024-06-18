712c411d4aa1507687dc00602ff735925a51e8ac
BeoGonzalez <beo.gonzalez@duocuc.cl>
2024-06-17 23:26:00 -0400

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,30 +1,24 @@
 import funciones as funcion
 import time
 
-
+trabajadores = []
 while True:
-    print("\nBienvenido a la empresa\n")
-    print("1.- Agregar trabajador.")
-    print("2.- Mostrar trabajadores.")
-    print("3.- Imprimir planilla de sueldos.")
-    print("4.- Salir del programa.\n")
-    try:
-        opcion=int(input("Ingrese una opción: "))
-    except ValueError:
-        print("\nError, ingrese un numero del 1 al 4.\n")
+    print("\n----- Menú Principal -----")
+    print("1) Registrar trabajador")
+    print("2) Listar todos los trabajadores")
+    print("3) Imprimir planilla de sueldos")
+    print("4) Salir del programa")
+        
+    opcion = input("Seleccione una opción: ")
+        
+    if opcion == '1':
+        funcion.registrar_trabajador()
+    elif opcion == '2':
+        funcion.listar_trabajadores()
+    elif opcion == '3':
+        funcion.imprimir_planilla()
+    elif opcion == '4':
+        print("Saliendo del programa...")
+        break
     else:
-        if (opcion==1):
-            funcion.agregarTrabajador()
-        elif (opcion==2):
-            funcion.mostrarTrabajador()
-            print("")
-        elif (opcion==3):
-            funcion.imprimir()
-        elif (opcion==4):
-            print("Saliendo del programa...")
-            time.sleep(2)
-            break
-        else:
-            print("Ingrese un numero valido..")
-
-
+        print("Opción no válida. Por favor seleccione una opción del menú.")
